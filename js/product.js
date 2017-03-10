fe6101f49f2d773d6a7320b44b0a31735881c525
Kevin Louzoun <louzou_k@epita.fr>
2017-03-10 15:36:33 +0100

    add product.js

---
+++ js/product.js
@@ -0,0 +1,40 @@
+function call_hackernews(page_number){
+    $.ajax({
+        'type': 'get',
+        'dataType': 'xml',
+        'url': 'https://news.ycombinator.com/rss?p="' + page_number +'',
+        success: function(response){
+            var channel_response = $(response).find("item")
+            var title, url;
+            $('.HackerNews').append('======== Hacker News ========<br/>');
+            $(channel_response).each(function(index, value) {
+                title = $(value).children('title').text();
+                url = $(value).children('link').text();
+                $('.HackerNews').append('<a href="'+ url +'"> [HN]: ' + title + ' </a><br/>');
+            });
+        }
+    });
+}
+
+function call_producthunt() {
+    $.ajax({
+        'type': 'get',
+        'dataType': 'xml',
+        'url': 'https://www.producthunt.com/feed',
+        success: function(response){
+            var title, url;
+            var product_response = $(response).find('entry');
+            $('.ProductHunt').append('======== Product Hunt ========<br/>');
+            $(product_response).each(function(index,value){
+                title = $(value).children('title').text();
+                url = $(value).find('link').attr('href');
+                $('.ProductHunt').append('<a href="'+ url +'"> [PH]: ' + title + ' </a><br/>');
+            });
+            console.log(product_response);
+        }
+    });
+}
+
+
+call_hackernews(1);
+call_producthunt();
